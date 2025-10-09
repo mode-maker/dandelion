3a96e53197c6742c9425644d428797a3b465b8c3
mode-maker <vladiclavm26reg.85@gmail.com>
2025-10-09 04:39:04 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,6 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Impressions from "../components/Impressions";
-
-export default function Page() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <Impressions />
-      </main>
-    </>
-  );
-}
 import WorkshopsGrid from "../components/WorkshopsGrid";
 
 export default function Page() {
