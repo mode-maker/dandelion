730fd093624dc83c2ac79b66ea64b034c56381e1
mode-maker <vladiclavm26reg.85@gmail.com>
2025-10-09 13:36:51 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Impressions from "../components/Impressions";
 import WorkshopsGrid from "../components/WorkshopsGrid";
+import ReadyTerrariums from "../components/ReadyTerrariums";
 
 export default function Page() {
   return (
@@ -11,6 +12,7 @@ export default function Page() {
         <Hero />
         <Impressions />
         <WorkshopsGrid />
+        <ReadyTerrariums />   {/* ← новый блок */}
       </main>
     </>
   );
