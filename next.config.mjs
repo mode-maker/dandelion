6ae18dafed9f3892d6554d4f7e4b2d0045e4db8d
mode-maker <vladiclavm26reg.85@gmail.com>
2025-10-17 02:57:37 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Заголовки для всех файлов из /public/events/*
+  async headers() {
+    return [
+      {
+        source: "/events/:file*",
+        headers: [
+          // Кэш на уровне CDN (s-maxage) — 1 год. В браузере max-age=0 (пусть решает CDN).
+        { key: "Cache-Control", value: "public, max-age=0, s-maxage=31536000, immutable" },
+          // Разрешим запросы диапазонов — корректная перемотка <video>.
+          { key: "Accept-Ranges", value: "bytes" },
+        ],
+      },
+    ];
+  },
 };
+
 export default nextConfig;
