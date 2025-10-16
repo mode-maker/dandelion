c8a1056863260c035616026580e48613289c5acd
mode-maker <vladiclavm26reg.85@gmail.com>
2025-10-17 03:08:11 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,23 @@ import Impressions from "../components/Impressions";
 import WorkshopsGrid from "../components/WorkshopsGrid";
 import ReadyTerrariums from "../components/ReadyTerrariums";
 import Events from "../components/Events";
+import Certificate from "../components/Certificate";
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Impressions />
+        <WorkshopsGrid />
+        <ReadyTerrariums />
+        <Events />
+        <Certificate />   {/* ← блок сертификата */}
+      </main>
+    </>
+  );
+}
 
 export default function Page() {
   return (
@@ -19,3 +36,4 @@ export default function Page() {
     </>
   );
 }
+
