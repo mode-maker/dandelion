cc06733970ac396c4ac49fa9fcec517eecb0b107
mode-maker <vladiclavm26reg.85@gmail.com>
2025-10-17 20:27:09 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Page() {
         <ReadyTerrariums />
         <Events />
         <Certificate />
-        <Footer /> {/* ⬅ футер */}
+        <Footer />
       </main>
     </>
   );
