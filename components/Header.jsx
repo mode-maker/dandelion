cf79d0f0bb41e9f30df6c6e67f7e6698c64cd8c3
mode-maker <vladiclavm26reg.85@gmail.com>
2025-10-17 20:32:49 +0600

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,7 @@
+"use client";
+import { FaTelegramPlane, FaInstagram } from "react-icons/fa";
+// ...остальной код Header без изменений
+
 import { FaTelegramPlane, FaInstagram } from "react-icons/fa";
 
 export default function Header() {
