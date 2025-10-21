64972bcbab1b490a6aee7afdef0b2041274e01e2
mode-maker <vladiclavm26reg.85@gmail.com>
2025-10-21 09:56:53 +0600

    Update page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -28,3 +28,9 @@ async function onUpload(e) {
     await load(); // обновим список
   }
 }
+'use client';
+
+// запрет статической генерации и кэширования
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
